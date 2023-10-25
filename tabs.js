442bd698c72d16f48df6341e48a81428257373d1
ingasafranova <sfrnv@yahoo.co.uk>
2023-10-25 12:44:19 +0100

    changed tab navigation to links

--- tabs.js
+++ tabs.js
@@ -1,44 +1,51 @@
-const tabLists = document.querySelector('[role="tablist"]');
-const tabs = tabLists.querySelectorAll('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
 
 let tabFocus = 0;
-
-//  tabindex makes element focusable, allow or prevent navigation with Tab 
-// 0 - means focused 
-// -1 - not focused
-
-tabLists.addEventListener('keydown', changeTabFocus )
-    
-   function changeTabFocus(event){
-    // all keys has they numbers
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
-
-
-    // change the tabindex of the current tab to -1
-    if (event.keyCode === keydownLeft || event.keyCode === keydownRight) {
-        tabs[tabFocus].setAttribute('tabindex', -1);
+    
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
     }
     
-    // if the right key is pushed, move to the next tab on the right
-    if(event.keyCode === keydownRight){
+    if (e.keyCode === keydownRight) {
         tabFocus++;
-        if (tabFocus >= tabs.length){
+        if (tabFocus >= tabs.length) {
             tabFocus = 0;
         }
-        console.log(tabFocus)
     }
-    // if the left key is pushed, move to the next tab on the left
-if (event.keyCode === keydownLeft){
-    tabFocus--;
-    if (tabFocus < 0 ){
-        tabFocus = tabs.length -1
+    
+    if (e.keyCode === keydownLeft) {
+        tabFocus--;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
     }
+    
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
 }
 
-tabs[tabFocus].setAttribute('tabindex', 0);
-tabs[tabFocus].focus();
-}
- 
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
     
-    
+            // const tabContainer = targetTab.parentNode;
+            // const mainContainer = tabContainer.parentNode;
+            
+            // mainContainer
+            //     .querySelectorAll('[role="tabpanel"]')
+            //     .forEach((panel) => panel.setAttribute("hidden", true));
+            
+            // mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+}                                   
